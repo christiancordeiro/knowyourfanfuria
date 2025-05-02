e45d8ec3e6296f0ac2ee19ff6b3fd91339b6a035
christiancordeiro <christiancordeiro96@gmail.com>
2025-05-02 15:51:38 -0300

    add: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect, useState } from "react"
 import Carteirinha from "./Components/Carteirinha/Carteirinha"
 import Home from "./Components/Home/Home"
 import { UserStorage } from "./UserContext"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
-  const [path, setPath] = useState(window.location.pathname)
-
-  useEffect(() => {
-    // Atualiza o estado do caminho sempre que o usuário navegar
-    const handlePopState = () => setPath(window.location.pathname)
-    window.addEventListener("popstate", handlePopState)
-
-    return () => window.removeEventListener("popstate", handlePopState)
-  }, [])
-
-  // Renderiza o componente baseado na URL
-  const renderPage = () => {
-    if (path === "/carteirinha") {
-      return <Carteirinha />
-    }
-    return <Home />
-  }
-
   return (
     <div>
-      <UserStorage>{renderPage()}</UserStorage>
+      <BrowserRouter>
+        <UserStorage>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/carteirinha" element={<Carteirinha />} />
+          </Routes>
+        </UserStorage>
+      </BrowserRouter>
     </div>
   )
 }
